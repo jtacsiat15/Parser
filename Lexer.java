72343b0ee8a8775b87c2d47d3d88b4413d099285
JCAASSHH <jalentacsiat@gmail.com>
2020-02-06 13:33:41 -0800

    changes

---
+++ Lexer.java
@@ -0,0 +1,364 @@
+/**
+ * Author: Jalen Tacsiat
+ * Assign: 2
+ *
+ * The lexer implementation tokenizes a given input stream. The lexer
+ * implements a pull-based model via the nextToken function such that
+ * each call to nextToken advances the lexer to the next token (which
+ * is returned by nextToken). The file has been completed read when
+ * nextToken returns the EOS token. Lexical errors in the source file
+ * result in the nextToken function throwing a MyPL Exception.
+ */
+
+import java.util.*;
+
+import jdk.nashorn.internal.ir.Symbol;
+
+import java.io.*;
+
+
+public class Lexer {
+
+  private BufferedReader buffer; // handle to input stream
+  private int line;
+  private int column;
+  
+  /** 
+   */
+  public Lexer(InputStream instream) {
+    buffer = new BufferedReader(new InputStreamReader(instream));
+    this.line = 1;
+    this.column = 0;
+  }
+
+  
+  /**
+   * Returns next character in the stream. Returns -1 if end of file.
+   */
+  private int read() throws MyPLException {
+    try {
+      int ch = buffer.read();
+      return ch;
+    } catch(IOException e) {
+      error("read error", line, column + 1);
+    }
+    return -1;
+  }
+
+  
+  /** 
+   * Returns next character without removing it from the stream.
+   */
+  private int peek() throws MyPLException {
+    int ch = -1;
+    try {
+      buffer.mark(1);
+      ch = read();
+      buffer.reset();
+    } catch(IOException e) {
+      error("read error", line, column + 1);
+    }
+    return ch;
+  }
+
+
+  /**
+   * Print an error message and exit the program.
+   */
+  private void error(String msg, int line, int column) throws MyPLException {
+    throw new MyPLException("Lexer", msg, line, column);
+  }
+
+
+  /**
+   */
+  public Token nextToken() throws MyPLException {
+    // TODO: your job in HW2 is to implement the next token function
+    String lexeme = "";
+    char Symbol = (char)peek();
+    if(Character.isWhitespace((char)peek())){
+      if((char)read() == '\n'){
+        ++line;
+        column = 1;
+        //return nextToken();
+      }
+      else{
+        ++column;
+      }
+      return nextToken();
+    }
+    if((char)peek() == '#'){
+      read();
+      ++column;
+      while(!((char)read() == '\n'));
+      ++line;
+      column = 1;
+      return nextToken();
+    }
+    else if (peek() == -1){
+      return new Token(TokenType.EOS, "", line, column);
+    }
+    else if ((char)peek() == ',') {
+      read();
+      ++column;
+      return new Token(TokenType.COMMA, ",", line, column);
+    }
+    else if ((char)peek() == '.') {
+      read();
+      ++column;
+      return new Token(TokenType.DOT, ".", line, column);
+    }
+    else if ((char)peek() == '+') {
+      read();
+      ++column;
+      return new Token(TokenType.PLUS, "+", line, column);
+    }
+    else if ((char)peek() == '-') {
+      read();
+      ++column;
+      return new Token(TokenType.MINUS, "-", line, column);
+    }
+    else if ((char)peek() == '*') {
+      read();
+      ++column;
+      return new Token(TokenType.MULTIPLY, "*", line, column);
+    }
+    else if ((char)peek() == '/') {
+      read();
+      ++column;
+      return new Token(TokenType.DIVIDE, "/", line, column);
+    }
+    else if ((char)peek() == '%') {
+      read();
+      ++column;
+      return new Token(TokenType.MODULO, "%", line, column);
+    }
+    else if ((char)peek() == '>') {
+      read();
+      ++column;
+      if(peek() == '='){
+        read();
+        ++column;
+        return new Token(TokenType.GREATER_THAN_EQUAL, ">=", line, column - 1);
+      }
+      else
+      return new Token(TokenType.GREATER_THAN, ">", line, column);
+    }
+    else if ((char)peek() == '<') {
+      read();
+      ++column;
+      if(peek() == '='){
+        read();
+        ++column;
+        return new Token(TokenType.LESS_THAN_EQUAL, "<=", line, column - 1 );
+      }
+      else
+      return new Token(TokenType.LESS_THAN, "<", line, column);
+    }
+    else if ((char)peek() == '=') {
+      read();
+      ++column;
+      return new Token(TokenType.EQUAL, "=", line, column);
+    }
+    else if ((char)peek() == '!') {
+      read();
+      ++column;
+      if ((char)peek() == '=') {
+        read();
+        ++column;
+        return new Token(TokenType.NOT_EQUAL, "!=", line, column - 1);
+      }
+    }
+    else if ((char)peek() == '(') {
+      read();
+      ++column;
+      return new Token(TokenType.LPAREN, "(", line, column);
+    }
+    else if ((char)peek() == ')') {
+      read();
+      ++column;
+      return new Token(TokenType.RPAREN, ")", line, column);
+    }
+    else if ((char)peek() == ':') {
+      read();
+      ++column;
+      if((char)peek() == '=')
+      read();
+      ++column;
+      return new Token(TokenType.ASSIGN, ":=", line, column - 1);
+    }
+    else if((char)peek() == '\''){
+      read();
+      ++column;
+      while(peek() != '\''){
+        //Symbol = (char)read();
+        lexeme += (char)read();
+      }
+      read();
+      ++column;
+      return new Token(TokenType.CHAR_VAL, lexeme, line, column - 1);
+    }
+    else if((char)peek() == '"'){
+      read();
+      ++column;
+      int columnStart = column;
+      //int col = column;
+      while((char)peek() != '"'){
+        //Symbol = (char)read();
+        if (peek() == -1){
+          String msg = "Bad string need to close string declaration with quotes";
+          error(msg, line, column);
+        }
+        if((char)peek() == '\n'){
+          String msg = "found newline within string";
+          error(msg, line, column);
+        }
+        else
+          lexeme += (char)read(); 
+      }
+      read();
+      ++column;
+      return new Token(TokenType.STRING_VAL, lexeme, line, columnStart);
+    }
+    else if(Character.isDigit(Symbol)){
+      //lexeme += Symbol;
+      int columnStart = 0;
+      while(Character.isDigit((char)peek())){
+        lexeme+=(char)read();
+        ++column;
+        columnStart = column;
+        if(Character.isLetter((char)peek())){
+          //++column;
+          String msg = "unexpected symbol '" + (char)peek() + "'";
+          error(msg, line, column );
+        }
+        if((char)peek() == '.'){
+            lexeme += (char)read();
+            if(!Character.isDigit((char)peek())){
+              String msg = "missing digit in float '" + lexeme + "'";
+              error(msg, line, column);
+            }
+            else{
+            while(Character.isDigit((char)peek())){
+              lexeme += (char)read();
+            }
+            if(Character.isLetter((char)peek())){
+              String msg = "invalid character in " + lexeme;
+              error(msg, line, column);
+            }
+            else if((char)peek() == '.'){
+              String msg = "Too many decimals in number " + lexeme;
+              error(msg, line, column);
+            }
+          }
+            return new Token(TokenType.DOUBLE_VAL, lexeme, line, column);
+        }
+        else if(!Character.isDigit((char)peek())){  
+        if(lexeme.charAt(0) == '0' && lexeme.length() > 1){
+          String msg = "leading zero in " + "'" + lexeme + "'";
+          error(msg, line, column);
+        }
+        return new Token(TokenType.INT_VAL, lexeme, line, columnStart);
+      }
+    }
+  }
+    //read strings in file
+    if(Character.isLetter(Symbol)){
+      //System.out.println(lexeme);
+      ++column;
+      int columnStart = column;
+      while(!Character.isWhitespace((char)peek()) 
+            && !((char)peek() == '\n') 
+            && !((char)peek() == '(') 
+            && !((char)peek() == ')') 
+            && !((char)peek() == '!') 
+            && !((char)peek() == '<')
+            && !((char)peek() == '>')
+            && !((char)peek() == '=')
+            && !((char)peek() == ':') ){
+        lexeme += (char)read();
+        ++column;
+      }
+      //System.out.println(lexeme);
+      if(lexeme.equals("int")){
+        return new Token(TokenType.INT_TYPE, lexeme, line, columnStart);
+      }
+      else if(lexeme.equals("bool")){
+        return new Token(TokenType.BOOL_TYPE, lexeme, line, columnStart);
+      }
+      else if(lexeme.equals("double")){
+        return new Token(TokenType.DOUBLE_TYPE, lexeme, line, columnStart);
+      }
+      else if(lexeme.equals("char")){
+        return new Token(TokenType.CHAR_TYPE, lexeme, line, columnStart);
+      }
+      else if(lexeme.equals("string")){
+        return new Token(TokenType.STRING_TYPE, lexeme, line, columnStart);
+      }
+      else if(lexeme.equals("type")){
+        return new Token(TokenType.TYPE, lexeme, line, columnStart);
+      }
+      else if(lexeme.equals("and")){
+        return new Token(TokenType.AND, lexeme, line, columnStart);
+      }
+      else if(lexeme.equals("or")){
+        return new Token(TokenType.OR, lexeme, line, columnStart);
+      }
+      else if(lexeme.equals("not")){
+        return new Token(TokenType.NOT, lexeme, line, columnStart);
+      }
+      else if(lexeme.equals("neg")){
+        return new Token(TokenType.NEG, lexeme, line, columnStart);
+      }
+      else if(lexeme.equals("while")){
+        return new Token(TokenType.WHILE, lexeme, line, columnStart);
+      }
+      else if(lexeme.equals("for")){
+        return new Token(TokenType.FOR, lexeme, line, columnStart);
+      }
+      else if(lexeme.equals("to")){
+        return new Token(TokenType.TO, lexeme, line, columnStart);
+      }
+      else if(lexeme.equals("do")){
+        return new Token(TokenType.DO, lexeme, line, columnStart);
+      }
+      else if(lexeme.equals("if")){
+        return new Token(TokenType.IF, lexeme, line, columnStart);
+      }
+      else if(lexeme.equals("then")){
+        return new Token(TokenType.THEN, lexeme, line, columnStart);
+      }
+      else if(lexeme.equals("else")){
+        return new Token(TokenType.ELSE, lexeme, line, columnStart);
+      }
+      else if(lexeme.equals("elif")){
+        return new Token(TokenType.TYPE, lexeme, line, columnStart);
+      }
+      else if(lexeme.equals("end")){
+        return new Token(TokenType.END, lexeme, line, columnStart);
+      }
+      else if(lexeme.equals("fun")){
+        return new Token(TokenType.FUN, lexeme, line, columnStart);
+      }
+      else if(lexeme.equals("var")){
+        return new Token(TokenType.VAR, lexeme, line, columnStart);
+      }
+      else if(lexeme.equals("set")){
+        return new Token(TokenType.SET, lexeme, line, columnStart);
+      }
+      else if(lexeme.equals("return")){
+        return new Token(TokenType.RETURN, lexeme, line, columnStart);
+      }
+      else if(lexeme.equals("new")){
+        return new Token(TokenType.NEW, lexeme, line, columnStart);
+      }
+      else if(lexeme.equals("nil")){
+        return new Token(TokenType.NIL, lexeme, line, columnStart);
+      }
+      else {
+        return new Token(TokenType.ID, lexeme, line, columnStart);
+      }
+    }
+    return new Token(TokenType.EOS, "", line, column);  
+  }
+}
--- Parser.java
+++ Parser.java
@@ -1,5 +1,5 @@
 /**
- * Author: 
+ * Author: Jalen Tacsiat
  * Homework: #3
  * File: Parser.java
  * 
@@ -15,7 +15,7 @@ public class Parser {
     
   private Lexer lexer; 
   private Token currToken = null;
-  private boolean debug_flag = false;  // set to false to remove debug comments
+  private boolean debug_flag = true;  // set to false to remove debug comments
   
   /** 
    * Create a new parser over the given lexer.
@@ -73,18 +73,452 @@ public class Parser {
   
   // <stmts> ::= <stmt> <stmts> | epsilon
   private void stmts() throws MyPLException {
-    debug("<stmts>");
-    // TODO
+    if(currToken.type() != TokenType.EOS){
+      debug("<stmts>");
+      stmt();
+      stmts();
+    }
   }
 
 
   // <bstmts> ::= <bstmt> <bstmts> | epsilon
   private void bstmts() throws MyPLException {
-    debug("<bstmts>");
-    // TODO
+    System.out.println(currToken.type());
+    if(isBstmt(currToken.type())){
+      debug("<bstmts>");
+      bstmt();
+      bstmts();
+    }
   }
   
-
   // TODO: remaining recursive descent functions ...
+
+  private void stmt() throws MyPLException{
+    debug("<stmt>");
+    if(currToken.type() == TokenType.TYPE){
+      tdecl();
+    }
+    else if(currToken.type() == TokenType.FUN){
+      fdecl();
+    }
+    else{
+      bstmt();
+    }
+  }
+
+  private void bstmt() throws MyPLException{
+    debug("<bstmt>");
+    if(currToken.type() == TokenType.VAR){
+      //advance();
+      vdecl();
+    }
+    else if(currToken.type() == TokenType.SET){
+      //advance();
+      assign();
+    }
+    else if(currToken.type() ==  TokenType.IF){
+      //advance();
+      cond();
+    }
+    else if(currToken.type() == TokenType.WHILE){
+      //advance();
+      tokenWhile();
+    }
+    else if(currToken.type() == TokenType.FOR){
+      //advance();
+      tokenFor();
+    }
+    else if(currToken.type() == TokenType.RETURN){
+      //advance();
+      exit();
+    }
+    else{
+      expr();
+    }
+  }
+
+  private void tdecl() throws MyPLException{
+    debug("<tdecl>");
+    eat(TokenType.TYPE, "expecting type" );
+    eat(TokenType.ID, "expecting ID" );
+    vdecls();
+    eat(TokenType.END, "expecting end");
+  }
+
+  private void vdecls() throws MyPLException{
+    debug("<vdecls>");
+    if(currToken.type() == TokenType.VAR){
+      vdecl();
+      vdecls();
+    }
+  }
+
+  private void fdecl() throws MyPLException{
+    debug("<fdecl>");
+    eat(TokenType.FUN, "expected word: FUN");
+    //System.out.println(isType(currToken.type()));
+    /*if(currToken.type() == TokenType.NIL ){
+      eat(TokenType.NIL, "expecting nil");
+      //System.out.println("in nil check");
+    }*/
+    if(isType(currToken.type())){
+      dtype();
+    }
+    else{
+      eat(TokenType.NIL, "expecting nil");
+    }
+    //System.out.println("after checks");
+    eat(TokenType.ID, "expected ID");
+    //System.out.println("after checks");
+    eat(TokenType.LPAREN, "expected left parentheses");
+    params();
+    eat(TokenType.RPAREN, "expected right parentheses");
+    bstmts();
+    eat(TokenType.END, "expected END token");
+  }
+  private void params() throws MyPLException{
+    if(isType(currToken.type())){
+      debug("<params>");
+      dtype();
+      eat(TokenType.ID, "expected ID");
+      while (currToken.type() == TokenType.COMMA){
+        advance();
+        dtype();
+        eat(TokenType.ID, "expected id");
+      }
+    }
+  }
+  private void dtype() throws MyPLException{
+    debug("<dtype>");
+    if(currToken.type() == TokenType.INT_TYPE){
+      advance();
+      //eat(TokenType.INT_TYPE, "expected int type");
+    }
+    else if(currToken.type() == TokenType.DOUBLE_TYPE){
+      advance();
+      //eat(TokenType.DOUBLE_TYPE, "expected double type");
+    }
+    else if(currToken.type() == TokenType.BOOL_TYPE){
+      advance();
+      //eat(TokenType.BOOL_TYPE, "expected bool type");
+    }
+    else if(currToken.type() == TokenType.CHAR_TYPE){
+      advance();
+      //eat(TokenType.CHAR_TYPE, "expected char type");
+    }
+    else if(currToken.type() == TokenType.STRING_TYPE){
+      advance();
+      //eat(TokenType.STRING_TYPE, "expected type");
+    }
+    else if(currToken.type() == TokenType.ID){
+      advance();
+      //eat(TokenType.ID, "expected ID");
+    }
+  }
+  private void exit() throws MyPLException{
+    debug("<exit>");
+    eat(TokenType.RETURN, "expected 'return'");
+    if(currToken.type() !=TokenType.END){
+      expr();
+    }
+    //debug("in exit");
+  }
+
+  private void vdecl() throws MyPLException{
+    debug("<vdecl>");
+    eat(TokenType.VAR, "Expected VAR");
+    if(currToken.type() == TokenType.ID){
+      advance();
+      if(currToken.type() == TokenType.ID){
+        advance();
+        //eat(TokenType.ASSIGN, "expecting ':='");
+      }
+    }
+    else{
+      dtype();
+      eat(TokenType.ID, "Expected ID");
+    }
+    eat(TokenType.ASSIGN, "Expected Assignment statement");
+    expr();
+    //error("end of vdecl");
+  }
+
+  private void assign() throws MyPLException{
+    debug("<assign>");
+    eat(TokenType.SET, "expected set");
+    lvalue();
+    eat(TokenType.ASSIGN, "exepected assignment statement");
+    expr();
+  }
   
+  private void lvalue() throws MyPLException{
+    debug("<lvalue>");
+    eat(TokenType.ID, "expected ID");
+    while(currToken.type() == TokenType.DOT){
+      advance();
+      eat(TokenType.ID, "expected ID");
+    }
+  }
+  private void cond() throws MyPLException{
+    debug("<cond>");
+    eat(TokenType.IF, "expected 'if'");
+    expr();
+    eat(TokenType.THEN, "expected 'then'");
+    bstmts();
+    condt();
+    eat(TokenType.END, "expected 'end'");
+  }
+
+  private void condt() throws MyPLException{
+    if(currToken.type() == TokenType.ELIF){
+      debug("<condt>");
+      eat(TokenType.ELIF, "expected 'elif'");
+      expr();
+      eat(TokenType.THEN, "expected 'then'");
+      bstmts();
+      condt();
+    }
+    if(currToken.type() == TokenType.ELSE){
+      advance();
+      bstmts();
+    }
+  }
+  private void tokenWhile() throws MyPLException{
+    debug("<tokenwhile>");
+    eat(TokenType.WHILE, "expected 'while'");
+    expr();
+    eat(TokenType.DO, "expected 'do'");
+    bstmts();
+    eat(TokenType.END, "expected 'end'");
+  }
+  private void tokenFor() throws MyPLException{
+    debug("<for>");
+    eat(TokenType.FOR, "expected 'for'");
+    eat(TokenType.ID, "expected 'id'");
+    eat(TokenType.ASSIGN, "expected ':=' for assignment");
+    expr();
+    eat(TokenType.TO, "expected 'to'");
+    expr();
+    eat(TokenType.DO, "expected 'do'");
+    bstmts();
+    eat(TokenType.END, "expected 'end'");
+  }
+  //when checking if statements advance
+  private void expr() throws MyPLException{
+    debug("<expr>");
+    if(currToken.type() == TokenType.NOT){
+      advance();
+      expr();
+    }
+    else if(currToken.type() == TokenType.LPAREN){
+      advance();
+      expr();
+      eat(TokenType.RPAREN, "expected ')' to close parentheses");
+    }
+    else{
+      rvalue();
+    }
+    if(isOperator(currToken.type())){
+      advance();
+      expr();
+    }
+  }
+  private void operator() throws MyPLException{
+    debug("<operator>");
+    if(currToken.type() == TokenType.PLUS){
+      advance();
+      //eat(TokenType.PLUS, "expected '+'");
+    }
+    if(currToken.type() == TokenType.MINUS){
+      advance();
+      //eat(TokenType.MINUS, "expected '-'");
+    }
+    if(currToken.type() == TokenType.DIVIDE){
+      advance();
+      //eat(TokenType.DIVIDE, "expected '/'");
+    }
+    if(currToken.type() == TokenType.MULTIPLY){
+      advance();
+      //eat(TokenType.MULTIPLY, "expected '*'");
+    }
+    if(currToken.type() == TokenType.MODULO){
+      advance();
+      //eat(TokenType.MODULO, "expected '%'");
+    }
+    if(currToken.type() == TokenType.AND){
+      advance();
+      //eat(TokenType.AND, "expected 'and'");
+    }
+    if(currToken.type() == TokenType.OR){
+      advance();
+      //eat(TokenType.OR, "expected 'or'");
+    }
+    if(currToken.type() == TokenType.EQUAL){
+      advance();
+      //eat(TokenType.EQUAL, "expected '='");
+    }
+    if(currToken.type() == TokenType.LESS_THAN){
+      advance();
+      //eat(TokenType.LESS_THAN, "expected '<'");
+    }
+    if(currToken.type() == TokenType.GREATER_THAN){
+      advance();
+      //eat(TokenType.GREATER_THAN, "expected '>'");
+    }
+    if(currToken.type() == TokenType.LESS_THAN_EQUAL){
+      advance();
+      //eat(TokenType.LESS_THAN_EQUAL, "expected '<='");
+    }
+    if(currToken.type() == TokenType.GREATER_THAN_EQUAL){
+      advance();
+      //eat(TokenType.GREATER_THAN_EQUAL, "expected >=");
+    }
+    if(currToken.type() == TokenType.NOT_EQUAL){
+      advance();
+      //eat(TokenType.NOT_EQUAL, "expected '!='");
+    }
+
+  }
+  private void rvalue() throws MyPLException{
+    debug("<rvalue>");
+    if(currToken.type() == TokenType.NIL){
+      advance();
+    }
+    else if(currToken.type() == TokenType.NEW){
+      advance();
+      eat(TokenType.ID, "expected ID");
+    }
+    else if(currToken.type() ==  TokenType.NEG){
+      advance();
+      expr();
+    }
+    else if(currToken.type() == TokenType.ID){
+      idrval();
+    }
+    else{
+      pval();
+    }
+
+  }
+  private void pval() throws MyPLException{
+    debug("<pval>");
+    if(currToken.type() == TokenType.INT_VAL){
+      advance();
+      //eat(TokenType.INT_VAL, "expected integer value");
+    }
+    if(currToken.type() == TokenType.DOUBLE_VAL){
+      advance();
+      //eat(TokenType.DOUBLE_VAL, "expected double value");
+    }
+    if(currToken.type() == TokenType.BOOL_VAL){
+      advance();
+      //eat(TokenType.BOOL_VAL, "expected boolean value");
+    }
+    if(currToken.type() == TokenType.CHAR_VAL){
+      advance();
+      //eat(TokenType.CHAR_VAL, "expected char value");
+    }
+    if(currToken.type() == TokenType.STRING_VAL){
+      advance();
+      //eat(TokenType.STRING_VAL, "expected string value");
+    }
+    debug("in pval");
+
+  }
+  private void idrval() throws MyPLException{
+    debug("<idrval>");
+    //debug("before id eat");
+    eat(TokenType.ID, "expecting ID");
+    //debug("after id eat");
+    if(currToken.type() == TokenType.LPAREN){
+      advance();
+      //debug("in lparen");
+      exprlist();
+      eat(TokenType.RPAREN, "expecting ')'");
+    }
+    else{
+    while(currToken.type() == TokenType.DOT){
+      advance();
+      eat(TokenType.ID, "expecting ID");
+    }
+  }
+  }
+  private void exprlist() throws MyPLException{
+    debug("<exprlist>");
+    if(isExpr(currToken.type())){
+      expr();
+      while(currToken.type() == TokenType.COMMA){
+        advance();
+        expr();
+      }
+    }
+  }
+  public boolean isType(TokenType t){
+    Set <TokenType> s = new HashSet<TokenType>();
+    s.add(TokenType.INT_TYPE);
+    s.add(TokenType.DOUBLE_TYPE);
+    s.add(TokenType.BOOL_TYPE);
+    s.add(TokenType.CHAR_TYPE);
+    s.add(TokenType.STRING_TYPE);
+    s.add(TokenType.ID);
+    return s.contains(t);
+
+  }
+  public boolean isOperator(TokenType t){
+    Set <TokenType> s = new HashSet<TokenType>();
+    s.add(TokenType.PLUS);
+    s.add(TokenType.MINUS);
+    s.add(TokenType.DIVIDE);
+    s.add(TokenType.MULTIPLY);
+    s.add(TokenType.MODULO);
+    s.add(TokenType.AND);
+    s.add(TokenType.OR );
+    s.add(TokenType.EQUAL);
+    s.add(TokenType.LESS_THAN);
+    s.add(TokenType.GREATER_THAN);
+    s.add(TokenType.LESS_THAN_EQUAL);
+    s.add(TokenType.GREATER_THAN_EQUAL );
+    s.add(TokenType.NOT_EQUAL);       
+    return s.contains(t);
+  }
+  public boolean isExpr(TokenType t){
+    Set <TokenType> s = new HashSet<TokenType>();
+    s.add(TokenType.NIL);
+    s.add(TokenType.NEW);
+    s.add(TokenType.NEG);
+    s.add(TokenType.INT_VAL);
+    s.add(TokenType.DOUBLE_VAL);
+    s.add(TokenType.BOOL_VAL);
+    s.add(TokenType.CHAR_VAL);
+    s.add(TokenType.STRING_VAL);
+    s.add(TokenType.ID);
+    s.add(TokenType.LPAREN);
+    return s.contains(t);
+
+  }
+  public boolean isBstmt(TokenType t){
+    Set <TokenType> s = new HashSet<TokenType>();
+    s.add(TokenType.VAR);
+    s.add(TokenType.SET);
+    s.add(TokenType.IF);
+    s.add(TokenType.WHILE);
+    s.add(TokenType.FOR);
+    s.add(TokenType.RETURN);
+    s.add(TokenType.NOT);
+    s.add(TokenType.ASSIGN);
+    s.add(TokenType.IF);
+    s.add(TokenType.WHILE);
+    s.add(TokenType.FOR);
+    s.add(TokenType.NIL);
+    s.add(TokenType.NEW);
+    s.add(TokenType.NEG);
+    s.add(TokenType.INT_VAL);
+    s.add(TokenType.DOUBLE_VAL);
+    s.add(TokenType.BOOL_VAL);
+    s.add(TokenType.CHAR_VAL);
+    s.add(TokenType.STRING_VAL);
+    s.add(TokenType.ID);
+    s.add(TokenType.LPAREN);
+    return s.contains(t);
+  }
 }
